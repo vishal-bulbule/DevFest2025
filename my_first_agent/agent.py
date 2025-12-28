7f2c6140751c41699a76cefea77eeb3f92119066
Vishal Bulbule <vishalbulbule@Vishals-MacBook-Air.local>
2025-12-28 15:17:57 +0530

    workshop

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -1,8 +1,9 @@
 from google.adk.agents.llm_agent import Agent
-
+from google.adk.tools import google_search
 root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
     description='A helpful assistant for user questions.',
     instruction='You are helpful assistant to answer user queries.',
+    tools=[google_search],
 )
